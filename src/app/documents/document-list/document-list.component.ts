d723ba9dc6fbdd8df68ca31085860ae0510c0057
Amuleka <104656884+Amuleka@users.noreply.github.com>
2023-05-23 16:55:59 -0600

    Created a dynamic directive

--- src/app/documents/document-list/document-list.component.ts
+++ src/app/documents/document-list/document-list.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import {Document} from '../document.model';
 
 @Component({
   selector: 'app-document-list',
@@ -6,5 +7,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./document-list.component.css']
 })
 export class DocumentListComponent {
+  @Output() selectedDocumentEvent: EventEmitter<Document> = new EventEmitter<Document>();
 
+  documents: Document[] = [
+    new Document   ("1", "Document 1", "This is the first document", "https://example.com/document1", []),
+    new Document ("2", "Document 2", "This is the second document", "https://example.com/document2", []),
+    new Document ("3", "Document 3", "This is the third document", "https://example.com/document3", []),
+    new Document ("4", "Document 4", "This is the fourth document", "https://example.com/document4", []),
+    new Document ("5", "Document 5", "This is the fifth document", "https://example.com/document5", [])
+  ]
+
+  onSelectedDocument(document: Document) {
+    this.selectedDocumentEvent.emit(document);
+  }
 }
